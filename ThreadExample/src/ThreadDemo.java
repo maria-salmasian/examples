55f8f0900efad01f14b597cca29f1ef6cdb10efd
Maria Salmasian <maria.salmasian@gmail.com>
2020-09-12 18:24:05 +0400

    -added an example exercise about threads

---
+++ ThreadExample/src/ThreadDemo.java
@@ -0,0 +1,20 @@
+import java.util.Set;
+
+public class ThreadDemo {
+    public static void main(String[] args) throws InterruptedException {
+        int count = 0;
+        for (int i = 0; i <5 ; i++) {
+            Thread thread = new Thread(new ThreadExercise() );
+            thread.setName("I am number " + i);
+            System.out.println(thread.getName());
+            thread.run();
+            count++;
+
+        }
+        Thread.sleep(60000);
+        System.out.println("Thread count started by Main thread:"+ count);
+
+    }
+}
+
+
--- ThreadExample/src/ThreadExercise.java
+++ ThreadExample/src/ThreadExercise.java
@@ -1,2 +1,11 @@
-public class ThreadExercise {
+public class ThreadExercise implements Runnable{
+    @Override
+    public void run() {
+        System.out.println("I am a running thread");
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
